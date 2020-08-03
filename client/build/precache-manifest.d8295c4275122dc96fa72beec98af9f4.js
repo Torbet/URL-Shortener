5ee91cc349b5f3b4475e38c95889cb92c577b32e
Guy Torbet <61030227+Torbet@users.noreply.github.com>
2020-08-03 17:38:28 +0100

    https

---
+++ client/build/precache-manifest.d8295c4275122dc96fa72beec98af9f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dadab0f549525e490360c3d9cdb96766",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ff60a863106e5dc1a98",
+    "url": "/static/css/main.7de68fe7.chunk.css"
+  },
+  {
+    "revision": "bc269bbd687608a7d66f",
+    "url": "/static/js/2.594aeffd.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.594aeffd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ff60a863106e5dc1a98",
+    "url": "/static/js/main.d6269dbe.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
