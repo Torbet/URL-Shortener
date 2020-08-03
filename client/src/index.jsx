066234ef1b454ecb6ccdecfb5937955d42fade4c
Guy Torbet <61030227+Torbet@users.noreply.github.com>
2020-08-03 01:15:09 +0100

    Initial commit

---
+++ client/src/index.jsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import * as serviceWorker from './serviceWorker';
+
+import Button from '@material-ui/core/Button'
+import TextField from '@material-ui/core/TextField'
+
+import './styles.css'
+
+const inputStyle = {
+    width: '70%',
+    marginLeft: '15%',
+    marginTop: '2vh',
+    padding: '0.5%'
+}
+const buttonStyle = {
+    width: '20%',
+    height: '7vh',
+    marginLeft: '15.5%',
+    marginTop: '2vh',
+    fontSize: '150%'
+}
+
+const wshost = window.location.origin.replace(/^http/, 'ws')
+
+var socket = new WebSocket(whost);
+
+function App() {
+
+    const [value, setValue] = React.useState('')
+    const [slug, setSlug] = React.useState('')
+
+    function validURL(str) {
+        var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+          '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+          '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+          '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+          '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+          '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+        return !!pattern.test(str);
+    }
+
+    function handleChange(e) {
+        setValue(e.target.value)
+    }
+    function onClick() {
+        if (validURL(value) == true) {
+            socket.send(value)
+        }
+
+        else {
+            setSlug('please enter valid url')
+        }
+        
+    }
+
+    socket.onmessage = function(data) {
+        setSlug("https://url.torbet.co/" + data.data)
+    }
+
+    return (
+        <div>
+            <h1>URL Shortener</h1>
+            <TextField className='input' label="Enter URL:" variant="filled" style={inputStyle} inputProps={{style: {fontSize: '200%'}}} InputLabelProps={{style: {fontSize: '150%', padding: '0.5%'}}} onChange={(e) => handleChange(e)} />
+            <Button variant="contained" style={buttonStyle} onClick={onClick}>
+                Primary
+            </Button>
+            <h1>{slug}</h1>
+        </div>
+    )
+}
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
---
+++ server/server.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const ws = require('ws')
+const fs = require('fs')
+const path = require('path')
+
+const app = express()
+const port = process.env.PORT || 8000
+var data = JSON.parse(fs.readFileSync('./urls.json', 'utf8'))
+
+function getValue(value) {
+    return Object.keys(data).find(key => data[key] === value);
+}
+
+
+
+function addURL(slug, url) {
+    data = JSON.parse(fs.readFileSync('./urls.json', 'utf8'))
+
+    if (getValue(url) == null) {
+        data[slug] = url
+        fs.writeFileSync('./urls.json', JSON.stringify(data))
+    }
+}
+
+function getURL(slug) {
+    data = JSON.parse(fs.readFileSync('./urls.json', 'utf8'))
+    return(data[slug])
+}
+
+
+function genSlug() {
+    var result = '';
+    for (var i = 6; i > 0; --i) result += '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'[Math.floor(Math.random() * '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.length)];
+    return result;
+}
+
+
+app.use('/', express.static(path.join(__dirname, '../client/build')))
+
+app.get('/:slug', function(req, res) {
+    res.redirect(getURL((req.path).substr(1)))
+})
+
+var server = app.listen(port, console.log(`listening on port ${port}`))
+
+var wss = new ws.Server({ server }) 
+
+wss.on('connection', function(socket) {
+
+    socket.on('message', function(data) {
+        var slug = genSlug()
+        addURL(slug, data)
+        socket.send(getValue(data))
+    })
+})
